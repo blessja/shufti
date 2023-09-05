a055a8618054492c3fd1e3012b6070d7790c37a7
blessing <jaydenj772@gmail.com>
2023-09-05 11:50:22 +0200

    app js for new route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,6 +27,7 @@ function App() {
             <Route path='/:carWashId/user/dashboard/' element={<UserDashboard />} />
             <Route path='/staff/dashboard/:carWashId' element={<StaffDashboard />} />
             <Route path='/staff/dashboard/' element={<StaffDashboard />} />
+            <Route path='/users/:id/:carWashId' element={<UserDetails />} />
             <Route path='/users/:id' element={<UserDetails />} />
             <Route path='/profile' element={<ProfileEdit />} />
             <Route path='/register-customer/:carWashId' element={<RegistrationForm />} /> 
@@ -42,3 +43,43 @@ function App() {
 }
 
 export default App;
+
+
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import Header from './components/Header';
+// import Dashboard from './pages/Dashboard';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
+// import StaffLogin from './pages/StaffLogin';
+// import UserDashboard from './pages/UserDashboard';
+// import StaffDashboard from './pages/StaffDashboard';
+// import UserDetails from './components/UserDetails';
+// import ProfileEdit from './pages/ProfileEdit';
+
+// function App() {
+//   return (
+//     <>
+//       <Router>
+//         <div className='container'>
+//           <Header />
+//           <Routes>
+//             <Route path='/' element={<Dashboard />} />
+//             <Route path='/login' element={<Login />} />
+//             <Route path='/register' element={<Register />} />
+//             <Route path='/staff/login' element={<StaffLogin />} />
+//             <Route path='/user/dashboard/' element={<UserDashboard />} />
+//             <Route path='/staff/dashboard' element={<StaffDashboard />} />
+//             <Route path='/users/:id' element={<UserDetails />} />
+//             <Route path='/users/edit/:id' element={<ProfileEdit />} />
+//           </Routes>
+//         </div>
+//       </Router>
+//       <ToastContainer />
+//     </>
+//   );
+// }
+
+// export default App;
+
